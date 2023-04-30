7550c9ddb72ee0c6ba1198ce7172c724cffb07f7
JoeVan21 <jmv5650@psu.edu>
2023-04-30 19:45:21 -0400

    Update suggested-plan.js

--- src/suggested-plan.js
+++ src/suggested-plan.js
@@ -5,6 +5,10 @@ import { LitElement, html, css } from 'lit';
  export class SuggestedPlan extends LitElement {
   static properties = {
     header: { type: String },
+    weekNum: { type: String },
+    timeFrame: { type: String },
+    header: { type: String },
+    info: { type: String }
   }
 
   static styles = css`
@@ -15,15 +19,27 @@ import { LitElement, html, css } from 'lit';
 
   constructor() {
     super();
-    this.week = '1';
+    this.weekNum = '1';
+    this.timeFrame= '3 Hours';
+    this.header = 'Misconceptions about happiness';
+    this.info =
+      'In this module, you will learn what it means to be happy and why pursuing happiness is not a pointless endeavor. Dr. Santos addresses how our minds lie to us and how the science shows that our misconceptions about money, grades, and social media are holding us back from implementing the techniques studied in positive psychology.';
+  
   }
 
   render() {
     return html`
-    <div class="outline">
+    <div class="wrapper">
     <div class="weekly-syllabus">
-      <div class="week">Week ${this.week}</div>
-      <div class="week-info">${this.compTime}</div>
+      <div class="week">
+        <p>Week</p>
+        ${this.weekNum}
+      </div>
+      <div class="week-info">
+        ${this.timeFrame}
+        <p>${this.header}</p>
+        <p>${this.info}</p>
+      </div>
     </div>
   </div>
     `;
