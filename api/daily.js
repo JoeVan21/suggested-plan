0f894b142990bc221eef741dfc4042c9abd751f0
JoeVan21 <jmv5650@psu.edu>
2023-05-03 22:31:17 -0400

    update

--- api/daily.js
+++ api/daily.js
@@ -2,25 +2,25 @@ export default async function handler(request, res) {
 
     const daily = [
         {
-            "weekNum": "1",
-            "timeFrame": "2",
-            "header": "Introduction",
-            "info": "Why take this course?",
-            "summary": "9 Videos (20 minutes total) 5 Readings"
+            weekNum: "1",
+            timeFrame: "2",
+            header: "Introduction",
+            info: "Why take this course?",
+            summary: "9 Videos (20 minutes total) 5 Readings"
         },
         {
-            "weekNum": "1",
-            "timeFrame": "2",
-            "header": "Introduction",
-            "info": "Why take this course?",
-            "summary": "9 Videos (20 minutes total) 5 Readings"
+            weekNum: "1",
+            timeFrame: "2",
+            header: "Introduction",
+            info: "Why take this course?",
+            summary: "9 Videos (20 minutes total) 5 Readings"
         },
         {
-            "weekNum": "1",
-            "timeFrame": "2",
-            "header": "Introduction",
-            "info": "Info",
-            "summary": "9 Videos (20 minutes total) 5 Readings"
+            weekNum: "1",
+            timeFrame: "2",
+            header: "Introduction",
+            info: "Info",
+            summary: "9 Videos (20 minutes total) 5 Readings"
         }
     ];
     res.setHeader('Cache-Control', 'max-age=0, s-maxage=1800');
